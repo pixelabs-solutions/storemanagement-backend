ed3eff3bb237889ae64c86201dfa193f45eb8a1e
sohaibturk <sohaibajmalturk@gmail.com>
2024-05-08 16:17:57 +0500

    customer details

--- app/Controllers/CustomerController.php
+++ app/Controllers/CustomerController.php
@@ -1,14 +1,16 @@
 <?php 
 
 namespace Pixelabs\StoreManagement\Controllers;
-use Pixelabs\StoreManagement\Models\Base;
+use Pixelabs\StoreManagement\Models\Customer;
 
 class CustomerController
 {
     private $table_name = 'customers';
     public function index()
     {
-        $customers = Base::get_all($this->table_name);
-        include_once __DIR__ . '/../Views/customers/index.php';
+        $customers = Customer::get_customers();
+
+        print_r($customers);
+        // include_once __DIR__ . '/../Views/customers/index.php';
     }
 }
