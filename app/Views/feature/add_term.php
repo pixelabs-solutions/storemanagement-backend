0f63c248e403d0c89ed8a5235bf28ee0c3153b3a
atifshahzad322 <shahzadatif764@gmail.com>
2024-05-13 14:43:17 +0500

    add term and add feature form submit  done

--- app/Views/feature/add_term.php
+++ app/Views/feature/add_term.php
@@ -101,19 +101,22 @@
             <div class="col-12 col-md-12">
                 <div class="card">
 
-                    <form action="" method="post" class="card-body">
+                    <form class="card-body">
                         <!-- header -->
                         <div class="row gx-3">
                             <div class="col-md-6 mb-3">
-                                <label for="example-text-input" class="form-label"data-i18n="popoups.future_managment.edit_variation_in_product_managment.btn_addiotainal_term">the name of the term</label>
-                                <input type="text" class="form-control rounded-3 p-3" id="example-text-input"
+                                <label for="example-text-input" class="form-label"
+                                    data-i18n="popoups.future_managment.edit_variation_in_product_managment.btn_addiotainal_term">the
+                                    name of the term</label>
+                                <input type="text" class="form-control rounded-3 p-3" id="sms_term_name"
                                     style="background-color: #EAEAEA" placeholder="Pink">
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="example-select" class="form-label">associated feature </label>
                                 <!--surround the select box with a "custom-select" DIV element. Remember to set the width:-->
                                 <div class="sms_mu_bg_div rounded-2">
-                                    <select class="form-select form-select-md h-100 bg-transparent">
+                                    <select class="form-select form-select-md h-100 bg-transparent"
+                                        id="sms_feature_select">
                                         <option value="HTML">Add Feature</option>
                                         <option value="Jquery">Custom Features</option>
                                     </select>
@@ -133,22 +136,28 @@
                                             Choosing a color for the display of the term </h3>
                                     </div>
                                     <div class="p-2">
-                                        <div class="col-md-12 mb-3">
+                                        <!-- <div class="col-md-12 mb-3">
                                             <label for="" class="form-label">The name of the
                                                 term</label>
                                             <input type="text" class="form-control text-dark rounded-2 py-3 "
-                                                id="example-text-input" style="background-color: #EAEAEA"
+                                                id="sms_term_names" style="background-color: #EAEAEA"
                                                 placeholder="Pink">
+                                        </div> -->
+                                        <div class="mb-3 p-2 col-12 rounded-3 d-flex align-items-center justify-content-between"
+                                            style="background-color: #EAEAEA">
+                                            <label class="form-label">Color change</label>
+                                            <input type="color" class="form-control p-0 form-control-color"
+                                                id="sms_term_color" value="#206bc4" title="Choose your color">
                                         </div>
                                         <div class="col-md-12 mb-3">
                                             <label class="form-label">
                                                 <label class="form-label">Selecting an image to display the term</label>
                                                 <div class="sms_a_add_term_input">
-                                                    <input type="file" id="image-input" accept="image/*"
+                                                    <input type="file" id="sms_term_image" accept="image/*"
                                                         style="background-color:#FFFFFF;"
                                                         onchange="sms_a_add_term_showFileName(this)">
-                                                    <label for="image-input"><i class="bi bi-image text-black"></i> <svg
-                                                            width="20" height="20" viewBox="0 0 32 26" fill="none"
+                                                    <label for="sms_term_image"><i class="bi bi-image text-black"></i>
+                                                        <svg width="20" height="20" viewBox="0 0 32 26" fill="none"
                                                             xmlns="http://www.w3.org/2000/svg">
                                                             <path
                                                                 d="M8.88889 0.555542C6.92778 0.555542 5.33333 2.14999 5.33333 4.1111V16.5555C5.33333 18.5167 6.92778 20.1111 8.88889 20.1111H28.4444C30.4056 20.1111 32 18.5167 32 16.5555V4.1111C32 2.14999 30.4056 0.555542 28.4444 0.555542H8.88889ZM22 6.48332L27.3333 14.4833C27.6056 14.8944 27.6333 15.4167 27.4 15.85C27.1667 16.2833 26.7167 16.5555 26.2222 16.5555H18.2222H15.5556H11.1111C10.6 16.5555 10.1333 16.2611 9.91111 15.8C9.68889 15.3389 9.75 14.7889 10.0722 14.3889L13.6278 9.94443C13.8833 9.62776 14.2611 9.44443 14.6667 9.44443C15.0722 9.44443 15.4556 9.62776 15.7056 9.94443L16.6667 11.1444L19.7778 6.47776C20.0278 6.1111 20.4444 5.88888 20.8889 5.88888C21.3333 5.88888 21.75 6.1111 22 6.48332ZM10.6667 5.88888C10.6667 5.41738 10.854 4.96519 11.1874 4.6318C11.5208 4.2984 11.9729 4.1111 12.4444 4.1111C12.9159 4.1111 13.3681 4.2984 13.7015 4.6318C14.0349 4.96519 14.2222 5.41738 14.2222 5.88888C14.2222 6.36037 14.0349 6.81256 13.7015 7.14595C13.3681 7.47935 12.9159 7.66665 12.4444 7.66665C11.9729 7.66665 11.5208 7.47935 11.1874 7.14595C10.854 6.81256 10.6667 6.36037 10.6667 5.88888ZM2.66667 5.44443C2.66667 4.70554 2.07222 4.1111 1.33333 4.1111C0.594444 4.1111 0 4.70554 0 5.44443V17.8889C0 22.0611 3.38333 25.4444 7.55556 25.4444H25.3333C26.0722 25.4444 26.6667 24.85 26.6667 24.1111C26.6667 23.3722 26.0722 22.7778 25.3333 22.7778H7.55556C4.85556 22.7778 2.66667 20.5889 2.66667 17.8889V5.44443Z"
@@ -169,7 +178,8 @@
                                         another term click here +</button>
                                 </div>
                                 <div class="text-center mt-2 p-2  ">
-                                    <button type="submit" class="btn btn-danger col-12 col-md-12 rounded-4 py-3">this is
+                                    <button type="button" class="btn btn-danger col-12 col-md-12 rounded-4 py-3"
+                                        onclick="submit_add_term()">this is
                                         submit button</button>
                                 </div>
                             </div>
@@ -196,13 +206,13 @@
             inputCount += 1;
             var container = document.getElementById('sms_a_add_new_term');
             var newInput = document.createElement('div');
-            var uniqueId = 'image-input-' + inputCount; // Generate unique ID
+            var uniqueId = 'sms_term_image-' + inputCount; // Generate unique ID
             newInput.classList.add('col-md-12', 'mb-3', 'p-0');
             newInput.innerHTML = `
         <div class="gx-3">
             <div class="col-md-6 mb-3">
-                <label for="example-text-input-${inputCount}" class="form-label">The name of the term</label>
-                <input type="text" class="form-control rounded-3 p-3" id="example-text-input-${inputCount}" style="background-color: #EAEAEA" placeholder="Pink">
+                <label for="${inputCount}" class="form-label">The name of the term</label>
+                <input type="text" class="form-control rounded-3 p-3" id="sms_term_names${inputCount}" style="background-color: #EAEAEA" placeholder="Pink">
             </div>
             <div class="rounded">
                 <h3 class="card-title text-black fs-4 fw-bold" style="font-size: 35px;">
@@ -210,7 +220,7 @@
             </div>
             <div class="mb-3 p-2 col-12 rounded-3 d-flex align-items-center justify-content-between" style="background-color: #EAEAEA">
                 <label class="form-label">Color change</label>
-                <input type="color" class="form-control p-0 form-control-color" id="example-text-input-${inputCount}" value="#206bc4" title="Choose your color">
+                <input type="color" class="form-control p-0 form-control-color" id="sms_term_colors${inputCount}" value="#206bc4" title="Choose your color">
             </div>
             <label class="form-label mt-4 fs-3 p-0">Selecting an image to display the term</label>
             <div class="sms_a_add_term_input">
@@ -240,6 +250,61 @@
         // document.addEventListener("DOMContentLoaded", function (event) {
         //     sms_a_add_new_term_input();
         // });
+
+        function submit_add_term() {
+            // Get the values of the existing inputs
+            let termName = document.getElementById('sms_term_name').value;
+            let featureSelect = document.getElementById('sms_feature_select').value;
+            let colorInput = document.getElementById('sms_term_color').value;
+            let imageFile = document.getElementById('sms_term_image').files[0];
+
+            // Create FormData object to store form data
+            let formData = new FormData();
+
+            // Append existing input values to FormData object
+            formData.append('termName', termName);
+            formData.append('featureSelect', featureSelect);
+            formData.append('colorInput', colorInput);
+            formData.append('imageFile', imageFile);
+
+            // Get the values of dynamically added inputs
+            let dynamicTermNames = document.querySelectorAll('[id^="sms_term_names"]');
+            let dynamicTermColors = document.querySelectorAll('[id^="sms_term_colors"]');
+            let dynamicTermImages = document.querySelectorAll('[id^="sms_term_image-"]');
+
+            // Append dynamically added input values to FormData object
+            dynamicTermNames.forEach((input, index) => {
+                formData.append('dynamicTermNames[]', input.value);
+            });
+
+            dynamicTermColors.forEach((input, index) => {
+                formData.append('dynamicTermColors[]', input.value);
+            });
+
+            dynamicTermImages.forEach((input, index) => {
+                formData.append('dynamicTermImages[]', input.files[0]);
+            });
+
+            // Send FormData object with fetch API
+            fetch('your_backend_endpoint', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => {
+                    if (response.ok) {
+                        console.log('Data submitted successfully');
+                        // Handle any success response from the server
+                    } else {
+                        console.error('Error submitting data:', response.statusText);
+                        // Handle any error response from the server
+                    }
+                })
+                .catch(error => {
+                    console.error('Network error occurred:', error);
+                    // Handle network errors
+                });
+        }
+
 
     </script>
 
