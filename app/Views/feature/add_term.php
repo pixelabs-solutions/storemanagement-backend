c910a6f21bc41b15216384a843f41df13f6d4280
atifshahzad322 <shahzadatif764@gmail.com>
2024-05-14 18:46:20 +0500

    add term pop show done

--- app/Views/feature/add_term.php
+++ app/Views/feature/add_term.php
@@ -250,6 +250,7 @@
         </div>
     </div>
 
+    
     <!-- input javascript code  -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <!-- <script src="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.jquery.min.js"></script> -->
