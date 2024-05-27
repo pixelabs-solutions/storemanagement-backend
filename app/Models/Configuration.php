682736e20c9c42e2344732d9ebade7c454b47043
sohaibturk <sohaibajmalturk@gmail.com>
2024-05-27 18:11:13 +0500

    response codes in configuration model

--- app/Models/Configuration.php
+++ app/Models/Configuration.php
@@ -12,6 +12,7 @@ class Configuration
         $user_id = Authentication::getUserIdFromToken();
         if($user_id == null || $user_id == "")
         {
+            http_response_code(401);
             return json_encode([
                 "message" => "User not authenticated.",
                 "status_code" => 401 
@@ -26,6 +27,7 @@ class Configuration
             if ($stmt->execute()) 
             {
                 $stmt->close();
+                http_response_code(201);
                 return json_encode([
                     "message" => "Configuration added successfully.",
                     "status_code" => 201 
@@ -35,6 +37,7 @@ class Configuration
             {
                 $stmt->close();
                 error_log("Error executing insert: " . $stmt->error);
+                http_response_code(500);
                 return json_encode([
                     "message" => "Failed to add configuration.",
                     "status_code" => 500 // Internal Server Error
@@ -44,6 +47,7 @@ class Configuration
         else 
         {
             error_log("Error preparing statement: " . $connection->error);
+            http_response_code(500);
             return json_encode([
                 "message" => "Error preparing SQL statement.",
                 "status_code" => 500 // Internal Server Error
