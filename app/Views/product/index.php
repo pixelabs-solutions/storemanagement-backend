7a2214a32c23f2e327f76b36bc6a9459ecf1dbb7
Wasif khan <khanwasif099@gmail.com>
2024-05-29 17:45:13 +0500

    fixing issue

--- app/Views/product/index.php
+++ app/Views/product/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../partials/header.php';
 
-
+var_dump($number_of_products)
 ?>
 <link href="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.min.css" rel="stylesheet" />
 <style>
@@ -313,8 +313,16 @@ require_once __DIR__ . '/../partials/header.php';
                                 <div class="-5" style="background-color: #eaeaea; position: relative; border-radius:12px; height:55px;">
                                     <div class="col-md-12 rounded-4 bg-transparent h-100 text-start">
                                         <select id="category_in_product" multiple style="width: 100%; padding-right: 20px; border: none; background: transparent; height:100%;">
-                                            <option value="NOSQL">Bags</option>
-                                            <option value="NodeJS">List</option>
+                                            <?php
+
+                                                foreach ($categories as $category) {
+                                                    // Access the "name" property of each category object
+                                                    ?>   
+                                                    <option value="<?php echo $category['name'] ?>"><?php echo $category['name']; ?></option>
+                                                    <?php
+                                                }
+
+                                            ?>
                                         </select>
                                         <span class="span_div">
                                             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -454,7 +462,7 @@ require_once __DIR__ . '/../partials/header.php';
                 </div>
                 <div class="row g-2">
                     <p class="col-auto ms-auto mt-5 mb-5 me-5 Sms_mu_for_Eng"><span class="fw-bold fs-2">Total products on the site:
-                        </span><span class="fs-2"> 450</span></p>
+                        </span><span class="fs-2"> <?php echo $number_of_products; ?></span></p>
                     <p class="col-auto ms-auto mt-5 mb-5 me-5 Sms_mu_for_hebrew"><span class="fw-bold fs-2">Total מוצרים באתר:
                         </span><span class="fs-2"> 450</span></p>
                 </div>
