4104a9666b32ee3c04a79c042baf7e372e30aae6
pixelabs-tech <official.pixelabs@gmail.com>
2024-06-11 17:50:06 +0500

    Set

--- app/Views/statistics/overview.php
+++ app/Views/statistics/overview.php
@@ -97,16 +97,16 @@ require_once __DIR__ . '/../partials/header.php';
             </li>
             <li class="nav-item mb-2">
                 <a href="#tabs_product" class="nav-link_stats sms_w_item_deactive_stats justify-content-center" data-bs-toggle="tab"
-                    style=" border-radius:20px; padding: 5px 30px; width:150px;  color:black;text-decoration: none"        data-i18n="statististics.tabs_in_static.tab_Products ">Products</a>
+                    style=" border-radius:20px; padding: 5px 30px; width:150px;  color:black;text-decoration: none"        data-i18n="statististics.tabs_in_static.tab_Products">Products</a>
             </li>
             <li class="nav-item mb-2">
                 <a href="#tabs_revenues" class="nav-link_stats sms_w_item_deactive_stats justify-content-center"
-                    data-bs-toggle="tab" style=" border-radius:20px; padding: 5px 30px; width:150px;     color:black;text-decoration: none"       data-i18n="statististics.tabs_in_static.tab_Revenues ">Revenues</a>
+                    data-bs-toggle="tab" style=" border-radius:20px; padding: 5px 30px; width:150px;     color:black;text-decoration: none"   data-i18n="statististics.tabs_in_static.tab_Revenues">Revenues</a>
 
             </li>
             <li class="nav-item mb-2">
                 <a href="#tabs_orders" class="nav-link_stats sms_w_item_deactive_stats justify-content-center" data-bs-toggle="tab"
-                    style=" border-radius:20px; padding: 5px 30px; width:150px;  color:black;text-decoration: none"       data-i18n="statististics.tabs_in_static.tab_Orders ">Orders</a>
+                    style=" border-radius:20px; padding: 5px 30px; width:150px;  color:black;text-decoration: none"       data-i18n="statististics.tabs_in_static.tab_Orders">Orders</a>
 
             </li>
 
